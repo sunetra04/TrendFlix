f9133025aab0734260cf820beb521b1e762609ea
sunetra <sunetra_kcse2022@msit.edu.in>
2025-08-11 23:25:46 +0530

    Add project files

---
+++ src/components/Search.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+const Search = ({searchTerm, setSearchTerm}) => {
+    return(
+        <div className='search'>
+            <div>
+                <img src="search.svg" alt="search" />
+                <input type="text"  
+                placeholder='search for a movie'
+                value={searchTerm} 
+                onChange={(e) => setSearchTerm(e.target.value)}
+                />
+            </div>
+        </div>
+    )
+}
+export default Search;
